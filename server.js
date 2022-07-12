ffbd990b63f83de7aa6f7d99d45159b780b262c0
Jacob Hannah <jacob.hannah164@gmail.com>
2022-07-12 11:14:34 -0400

    the table candidates is now queried and printed to the console

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//Connect to the MySQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your sql username
+        user: 'root',
+        //Your sql password
+        password: 'Kingdom_01',
+        database:'election'
+    },
+    console.log('Connected to the Database.')
+)
+
+//return all the data in the candidates table
+db.query('SELECT * FROM candidates', (err ,rows) => {
+    console.log(rows);
+})
 
 //default for if a requeest does not exist
 app.use((req, res) => {
     res.status(404).end();
 })
 
+//confirmation of server starting
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
