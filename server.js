710b7664f33a06ae04c3d5c613a19113b377c75f
Jacob Hannah <jacob.hannah164@gmail.com>
2022-07-13 08:30:27 -0400

    created api endpoint for creating,deleting, and viewing the candidates table in the election database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inputCheck = require('./utils/inputCheck.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
@@ -21,35 +22,83 @@ const db = mysql.createConnection(
 )
 
 //return all the data in the candidates table
-db.query('SELECT * FROM candidates', (err ,rows) => {
-    //console.log(rows);
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })
 })
 
 //select a single candidate from the table
-// db.query(`SELECT * FROM candidates WHERE id = ?`, 1, (err, row) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(row);
-// })
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
 
 //delete a candidate
-// db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+app.delete('/api/candidate/:id', (req, res) =>{
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.statusMessage(400).json({error: message});
+        }else if(!result.affectedRows){
+            res.json({
+                message: 'Candidate not found!'
+            })
+        }else{
+            res.json({
+                message: 'Candidate Deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+    })
+})
 
 //create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// db.query(sql, params, (err, result) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
+app.post('/api/candidate', ({body}, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if(errors){
+        res.status(400).json({error: errors})
+        return;
+    }
+
+    //if no errors were found in the input then add the candidate to the database
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.status(400).json({error: err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })
+    })
+})
 
 //default for if a requeest does not exist
 app.use((req, res) => {
