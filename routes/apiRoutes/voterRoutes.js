816a7b93196796dcedd2d2dbccb362656297e4d6
Jacob Hannah <jacob.hannah164@gmail.com>
2022-07-14 07:54:34 -0400

    added voter routes for CRUD operations

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const db = require('../../db/connection.js');
 const inputCheck = require('../../utils/inputCheck.js');
 
-router.get('./voters', (req,res) => {
-    const sql = `SELECT * FROM voters`;
+//retrieve all voters in alphabetical order
+router.get('/voters', (req,res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
 
     db.query(sql, (err, rows) => {
         if(err){
@@ -18,4 +19,93 @@ router.get('./voters', (req,res) => {
     })
 })
 
-module.export = router;
+//Retrieve a single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })
+})
+
+//create a new voter
+router.post('/voter', ({body}, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql =`INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.status(400).json({error: err.message})
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        })
+    })
+})
+
+//update a voters email
+router.put('/voter/:id', (req, res) => {
+    //Data Validation
+    const errors = inputCheck(req.body, 'email');
+    if(errors){
+        res.status(400).json({error: errors});
+        return;
+    }
+
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    db.query(sql, params, (err,result) => {
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }else if(!result.affectedRows){
+            res.json({message: 'Voter not found.'});
+        }else{
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            })
+        }
+    })
+})
+
+//delete a voter
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+
+    db.query(sql, req.params.id, (err, result) => {
+        if(err){
+            res.status(400).json({error: res.message});
+            return;
+        }else if(!result.affectedRows){
+            res.json({message: 'Voter not found'});
+        }else{
+            res.json({
+                message: 'Voter deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            })
+        }
+    })
+})
+
+module.exports = router;
