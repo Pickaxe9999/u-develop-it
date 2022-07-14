1b289c7a4f1492858abe3c93700992b1781f20b3
Jacob Hannah <jacob.hannah164@gmail.com>
2022-07-14 08:31:21 -0400

    created a routes for collecting party vote totals with party information

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('./candidateRoutes.js'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
